6166d319729e92159730dc7d82b884c648bbc39b
Steve Fernandes <93852415+Aspireve@users.noreply.github.com>
2024-01-12 21:21:29 +0530

    Add files via upload

---
+++ client/src/Pages/Logout.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import * as routes from '../constants/routes';
+import * as userAction from '../actions/userAction';
+
+const Logout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(userAction.Logout());
+  }, [dispatch]);
+
+  return <Redirect to={routes.HOME} />;
+};
+
+export default Logout;
